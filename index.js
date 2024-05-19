d3583ee95e9b15c1733efc60eefd23683f12f747
JohannaGam <164930877+JohannaGam@users.noreply.github.com>
2024-05-19 17:21:22 +0200

    add Weather API

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "a0fa4tbdafo43564612e4338dfba95b2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCity);
+
+searchCity("Vienna");
