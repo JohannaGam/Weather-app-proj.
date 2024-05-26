aed1c118873c448b6fe63e9959c00a711567dbaa
JohannaGam <164930877+JohannaGam@users.noreply.github.com>
2024-05-26 14:23:57 +0200

    make the forecast work

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ function changeCity(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCity);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "a0fa4tbdafo43564612e4338dfba95b2";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -76,26 +83,22 @@ function getForecast(city) {
 
 function changeForecast(response) {
   console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast-day">
-      <div class="forecast-day">${day}</div>
-    <div class = "forecast-icon"><img
-      src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
-      alt=""
-      width="50"
-    /></div>
+      <div class="forecast-day">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}" class = "forecast-icon"/>
     <div class="forecast-temperature">
-      <span class="temp-max">20° </span>
-      <span class="temp-min"> 10°</span>
+      <span class="temp-max">${Math.round(day.temperature.maximum)}°</span>
+      <span class="temp-min">${Math.round(day.temperature.minimum)}°</span>
     </div>
     </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
